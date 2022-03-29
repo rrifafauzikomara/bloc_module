3c9c2d3ea98e1c3884351e3f4ef41fa49864d663
rrifafauzikomara <rifafauzi6@gmail.com>
2022-03-30 01:43:54 +0700

    migration to flutter sdk version 2!

--- lib/bloc/movie_list_bloc.dart
+++ lib/bloc/movie_list_bloc.dart
@@ -1,13 +1,12 @@
-import 'package:rxdart/rxdart.dart';
-
 import 'package:network/network.dart';
+import 'package:rxdart/rxdart.dart';
 
 class MovieListBloc {
 
   final _repository = Repository();
   final _movieFetcher = PublishSubject<Movie>();
 
-  Observable<Movie> get allMovie => _movieFetcher.stream;
+  Stream<Movie> get allMovie => _movieFetcher.stream;
 
   fetchAllMovie() async {
     Movie movie = await _repository.fetchAllMovie();
